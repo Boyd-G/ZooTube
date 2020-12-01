1b67a5757350da5517f550473e5c1df6f542eb21
Michael Greene <michaelduffgreenejr@gmail.com>
2020-12-01 16:30:23 -0600

    1st database commit

---
+++ src/server/config/index.ts
@@ -0,0 +1,10 @@
+export default {
+
+    mysql:{
+        host: "localhost",
+        port: 3306,
+        user: "animalCams",
+        database: "AnimalCamsDB",
+        password: "password"
+    }
+}
