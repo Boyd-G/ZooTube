76e13ef19b9ab573f4a01d9b3f19511926818660
Anthony Suarez <antsuarez305@gmail.com>
2020-12-04 17:21:43 -0600

    bringing ant-branch up to date to merge w/ master

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Organization from './components/Organizations'
+import Organization from './Components/Organizations'
 // import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 
