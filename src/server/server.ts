4c255b610c200ba507e34e1d42d436f9f15717bc
Boyd-G <boydgraves1@gmail.com>
2020-12-06 13:04:39 -0600

    finished out middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,18 @@
 import * as express from 'express';
 import * as path from "path";
+import * as passport from 'passport';
+
+import './middleware/localstrategy';
+import './middleware/bearerstrategy';
+
 import apiRouter from './routes';
 
 const app = express();
 
 app.use("/api", apiRouter);
 app.use(express.static('public'));
+app.use(express.json());
+app.use(passport.initialize());
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
