cade49932b8cb9d546d51a0373db05719303d9b0
ZF4 <zfarmer313@gmail.com>
2020-12-06 11:40:47 -0600

    Updated homepage and animals page, added map

--- src/client/Components/Animals.tsx
+++ src/client/Components/Animals.tsx
@@ -20,9 +20,42 @@ const Animals: React.FC<AnimalProps> = (props: AnimalProps) => {
     return (
         <React.Fragment>
 
-            <div>
-                <div className="card-group">
-                    <div className="card">
+            <div className="animal-container">
+                <img src="https://i.imgur.com/joRpSs0.png" className="map" />
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://imgur.com/YtRHikg.png" alt="bottle" className="tiger" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://imgur.com/JZeJyxj.png" alt="bottle" className="grafe" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/Nbjh1Cy.png" alt="bottle" className="penguin" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/093I8kR.png" alt="bottle" className="otter" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/kK7qJIW.png" alt="bottle" className="dog" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/ftB1zDD.png" alt="bottle" className="elephant" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/PgnCmbW.png" alt="bottle" className="shark" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/TK4uTC1.png" alt="bottle" className="fish" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/ikhy3o0.png" alt="bottle" className="panda" />
+                </a>
+                <a href="https://www.google.com/" id="bottle" target="_blank">
+                    <img src="https://i.imgur.com/DN6NNSz.png" alt="bottle" className="sloth" />
+                </a>
+
+
+                {/* <div className="card bg-secondary second-container animal-photos">
+                    <div className="card-body">
                         <img src="./assets/animals/1_animal_penguin.jpg" className="card-img-top" alt="Picture of a penguin" />
                         <div className="card-body">
                             <h5 className="card-title">Penguin</h5>
@@ -30,23 +63,23 @@ const Animals: React.FC<AnimalProps> = (props: AnimalProps) => {
                             <p className="card-text"><small className="text-danger">LIVE</small></p>
                         </div>
                     </div>
-                    <div className="card">
-                        <img src="./assets/animals/2_animal_sand_tiger_shark.jpg" className="card-img-top" alt="Tiger sand shark image" />
+                    <div className="card bg-secondary second-container animal-photos">
                         <div className="card-body">
+                            <img src="./assets/animals/2_animal_sand_tiger_shark.jpg" className="card-img-top" alt="Tiger sand shark image" />
                             <h5 className="card-title">Tiger Sand Shark</h5>
                             <p className="card-text">Click here to see the awesome tiger sand shark!</p>
                             <p className="card-text"><small className="text-danger">LIVE</small></p>
                         </div>
                     </div>
-                    <div className="card">
-                        <img src="./assets/animals/4_animals_sloth.jpg" className="card-img-top" alt="sloth image" />
+                    <div className="card bg-secondary second-container animal-photos">
                         <div className="card-body">
+                        <img src="./assets/animals/4_animals_sloth.jpg" className="card-img-top" alt="sloth image" />
                             <h5 className="card-title">Sloth</h5>
                             <p className="card-text">Click here to watch the sloth!</p>
                             <p className="card-text"><small className="text-danger">LIVE</small></p>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </React.Fragment>
     )
--- src/client/Components/Home.tsx
+++ src/client/Components/Home.tsx
@@ -20,50 +20,43 @@ function Home() {
     return (
         <div className="container">
             <h3 className="ft">Featured Stream</h3>
-            <div className="card shadow-lg first" style={{ width: 750 }}>
+            <div className="card shadow-lg main-container bg-secondary" style={{ width: 750 }}>
                 <div className="card-body">
-                    <h5 className="card-title">Atlanta Zoo</h5>
-                    <p className="card-text">Panda</p>
+                    <h5 className="card-title text-light">Atlanta Zoo   - Panda Exhibit</h5>
+                    {/* <p className="card-text text-light">Panda</p> */}
                     <iframe width="696" height="346" src="https://www.youtube.com/embed/1bTw9xzhD1A" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                     <div className="allBtns">
-                        <a href="https://zooatlanta.org" target="_blank"><button className="btn btn-sm btn-outline-success m-2">More info!</button></a>
-                        <a href="https://give.zooatlanta.org/donate" target="_blank"><button className="btn btn-sm btn-outline-success m-2">Donate!</button></a>
+                        <a href="https://zooatlanta.org" target="_blank"><button className="btn btn-sm btn-success m-2">More info!</button></a>
+                        <a href="https://give.zooatlanta.org/donate" target="_blank"><button className="btn btn-sm btn-success m-2">Donate!</button></a>
                     </div>
                 </div>
             </div>
-            <div className="card bg-light second">
+            <div className="card bg-secondary second-container">
                 <div className="card-body">
-                <a className="navbar-brand">
-                    <div className="mission-image">
-                        <img src="https://imgur.com/BP5xKjZ.png" className="img-fluid" />
-                    </div>
-                </a>
+                    <a className="navbar-brand">
+                        <div className="mission-image">
+                            <img src="https://imgur.com/BP5xKjZ.png" className="img-fluid" />
+                        </div>
+                    </a>
                     {/* <h5 className="card-title">Our Mission Statement</h5> */}
-                    <p className="card-text">Zoos and rescues around the country and around the world provide an important service by conducting research, rescuing and rehabilitating animals, overseeing animal population management,
+                    <p className="card-text text-light">Zoos and rescues around the country and around the world provide an important service by conducting research, rescuing and rehabilitating animals, overseeing animal population management,
                     and providing education and entertainment to millions of people each year.  During the current global pandemic, zoos have seen a sharp decline in attendance.
                     Due to this there are less available resources to help home, rehabilitate, and care for these animals.
                     Our goal is to provide a way for people to engage, and donate, to these animals and organizations that we all love, from the safety of their own homes.</p>
                 </div>
             </div>
-            <div className="card bg-light third">
-                <div className="card-body">
-                    <p>FUN FACT: This panda goes to sleep during presentation</p>
-                    {/* <h5 className="card-title">Could put text here</h5> */}
-                    {/* <a className="navbar-brand">
-                    <div className="sm-logo">
-                        <img src="https://imgur.com/dlXCiM7.png" className="img-fluid" />
-                    </div>
-                </a><a className="navbar-brand">
-                    <div className="sm-logo">
-                        <img src="https://imgur.com/aaLP9XH.png" className="img-fluid" />
-                    </div>
-                </a><a className="navbar-brand">
-                    <div className="sm-logo">
-                        <img src="https://imgur.com/fjPwu61.png" className="img-fluid" />
-                    </div>
-                </a> */}
-
-
+            <div className="card bg-secondary third-container">
+                <div className="card-body row mx-1">
+                    <p className="text-light">Connect with us!</p>
+                    <a className="navbar-brand" href="https://www.tiktok.com/en/" target="_blank">
+                        <img src="https://imgur.com/dlXCiM7.png" className="img-fluid sm-logo" />
+                    </a>
+                    <a className="navbar-brand" href="https://www.facebook.com/" target="_blank">
+                        <img src="https://imgur.com/8ZzQbQl.png" className="img-fluid sm-logo" />
+                    </a>
+                    <a className="navbar-brand" href="https://www.instagram.com/" target="_blank">
+                        <img src="https://imgur.com/fjPwu61.png" className="img-fluid sm-logo" />
+                    </a>
                 </div>
             </div>
         </div>
