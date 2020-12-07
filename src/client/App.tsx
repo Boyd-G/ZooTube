0c3fef05cf34088a153dbbe173338f8b52ba69ce
Michael Greene <michaelduffgreenejr@gmail.com>
2020-12-07 17:06:10 -0600

    singleAnimal component working- streams component started- still need styling and finish streams

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar';
 import Animals from './Components/Animals';
 import SingleOrganization from './Components/SingleOrganization';
 import Organizations from './Components/Organizations';
+import SingleAnimal from './Components/SingleAnimal';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
@@ -15,7 +16,7 @@ const App: React.FC<IAppProps> = () => {
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/streams/animals" component={Animals} />
-				{/* <Route exact path="/streams/animals/:id" component={SingleAnimals} />  singleAnimals component not done yet*/}
+			 	<Route exact path="/streams/animals/:id" component={SingleAnimal} /> 
 				<Route exact path="/streams/organizations/:id"  component={SingleOrganization} />
 				<Route exact path="/streams/organizations"  component={Organizations} />
 			</Switch>
---
+++ src/client/Components/SingleAnimal.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link, RouteComponentProps } from "react-router-dom";
+import { animals } from "../AnimalTypes";
+
+
+
+const singleAnimal: React.FC<ISingleAnimalProps> = (props) => {
+    const [animal, setSingleAnimal] = React.useState<animals[]>([]);
+
+    React.useEffect(() => {
+        fetchSingleAnimal();
+    }, []);
+
+    const fetchSingleAnimal = async () => {
+        try {
+            let res = await fetch(`/api/streams/animals/${props.match.params.id}`)
+            let Animal: animals[] = await res.json();
+            // Animal.reverse();
+            setSingleAnimal(Animal);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <div className="container">
+            {animal.map((animal: animals, index) => (
+                <div key={index} className="card shadow-lg m-2">
+                    <div className="card-body">
+                    {/* <h4 className="card-title">{animal.animal}</h4> */}
+                    <h5 className="card-title">{animal.animalName}</h5>
+                    <img src={animal.animalImageUrl} alt={animal.animalName}/>
+                    {/* <iframe width="696" height="346" src={animal.camUrl} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
+                    <h5 className="card-title">{animal.animalFunfact}</h5>
+                        <Link to={`/streams/animals/${animal.id}`}>
+                            <button className="btn btn-sm btn-outline-dark float-right">Live Stream</button>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+interface ISingleAnimalProps extends RouteComponentProps<{id: string}> { };
+
+export default singleAnimal 
