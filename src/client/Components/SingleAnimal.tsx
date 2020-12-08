cff20f4f7fbdc734481cc96547d4f972fe4a5566
Michael Greene <michaelduffgreenejr@gmail.com>
2020-12-08 13:45:59 -0600

    styling issues should be working

--- src/client/Components/SingleAnimal.tsx
+++ src/client/Components/SingleAnimal.tsx
@@ -1,47 +1,59 @@
-import React from 'react';
+import React from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { animals } from "../AnimalTypes";
 
-
-
 const singleAnimal: React.FC<ISingleAnimalProps> = (props) => {
-    const [animal, setSingleAnimal] = React.useState<animals[]>([]);
+  const [animal, setSingleAnimal] = React.useState<animals[]>([]);
 
-    React.useEffect(() => {
-        fetchSingleAnimal();
-    }, []);
+  React.useEffect(() => {
+    fetchSingleAnimal();
+  }, []);
 
-    const fetchSingleAnimal = async () => {
-        try {
-            let res = await fetch(`/api/streams/animals/${props.match.params.id}`)
-            let Animal: animals[] = await res.json();
-            // Animal.reverse();
-            setSingleAnimal(Animal);
-        } catch (err) {
-            console.log(err)
-        }
+  const fetchSingleAnimal = async () => {
+    try {
+      let res = await fetch(`/api/streams/animals/${props.match.params.id}`);
+      let Animal: animals[] = await res.json();
+      // Animal.reverse();
+      setSingleAnimal(Animal);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return (
-        <div className="container">
-            {animal.map((animal: animals, index) => (
-                <div key={index} className="card shadow-lg m-2">
-                    <div className="card-body">
-                    {/* <h4 className="card-title">{animal.animal}</h4> */}
-                    <h5 className="card-title">{animal.animalName}</h5>
-                    <img src={animal.animalImageUrl} alt={animal.animalName}/>
-                    {/* <iframe width="696" height="346" src={animal.camUrl} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
-                    <h5 className="card-title">{animal.animalFunFact}</h5>
-                        <Link to={`/streams/animals/${animal.id}`}>
-                            <button className="btn btn-sm btn-outline-dark float-right">Live Stream</button>
-                        </Link>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="container w-75">
+      {animal.map((animal: animals, index) => (
+        <div key={index} className="card shadow-lg m-2 ">
+          <div className="card-body justify-content-center ">
+            <h1 className="card-title m-3">{animal.animalName}</h1>
+            <h5 className="card-title m-3">{animal.description}</h5>
+            <div className="d-flex justify-content-center align-items-center">
+              <iframe
+                width="696"
+                height="346"
+                src={animal.camUrl}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              ></iframe>
+            </div>
+            <h4 className="card-title">FUNFACT: {animal.animalFunFact}</h4>
+           
+            <a href={animal.supportUrl} target="_blank">
+              <button className="btn btn-sm btn-outline-dark m-2">
+                Donate!
+              </button>
+            </a>
+            <a href={animal.homeUrl} target="_blank">
+              <button className="btn btn-sm btn-outline-dark m-2 float-right">
+                More info!
+              </button>
+            </a>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
-interface ISingleAnimalProps extends RouteComponentProps<{id: string}> { };
+interface ISingleAnimalProps extends RouteComponentProps<{ id: string }> {}
 
-export default singleAnimal 
+export default singleAnimal;
--- src/client/Components/SingleOrganization.tsx
+++ src/client/Components/SingleOrganization.tsx
@@ -1,47 +1,59 @@
-import React from 'react';
+import React from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { organization } from "../OrgTypes";
 
-
-
 const singleOrganizations: React.FC<ISingleOrganizationProps> = (props) => {
-    const [organizations, setSingleOrganizations] = React.useState<organization[]>([]);
+  const [organizations, setSingleOrganizations] = React.useState<
+    organization[]
+  >([]);
 
-    React.useEffect(() => {
-        fetchSingleOrganizations();
-    }, []);
+  React.useEffect(() => {
+    fetchSingleOrganizations();
+  }, []);
 
-    const fetchSingleOrganizations = async () => {
-        try {
-            let res = await fetch(`/api/streams/organizations/${props.match.params.id}`)
-            let organizations: organization[] = await res.json();
-            // organizations.reverse();
-            setSingleOrganizations(organizations);
-        } catch (err) {
-            console.log(err)
-        }
+  const fetchSingleOrganizations = async () => {
+    try {
+      let res = await fetch(
+        `/api/streams/organizations/${props.match.params.id}`
+      );
+      let organizations: organization[] = await res.json();
+      // organizations.reverse();
+      setSingleOrganizations(organizations);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return (
-        <div className="container">
-            {organizations.map((organization: organization, index) => (
-                <div key={index} className="card shadow-lg m-2 style={{width:680}}">
-                    <div className="card-body">
-                    {/* <h4 className="card-title">{organization.organization}</h4> */}
-                    <h5 className="card-title text-light bg-secondary">{organization.animalName}</h5>
-                    <img src={organization.animalImageUrl} alt={organization.animalName}/>
-                    {/* <iframe width="696" height="346" src={organization.camUrl} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
-                    <h5 className="card-title">{organization.animalFunfact}</h5>
-                        <Link to={`/streams/animals/${organization.id}`}>
-                            <button className="btn btn-sm btn-outline-dark float-right">Live Stream</button>
-                        </Link>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="container w-75">
+      {organizations.map((organization: organization, index) => (
+        <div key={index} className="card shadow-lg m-2 w-75">
+          <div className="card-body justify-content-center ">
+            {/* <h4 className="card-title">{organization.organization}</h4> */}
+            <h5 className="card-title">{organization.animalName}</h5>
+            <div className="d-flex align-items-center ">
+              <img
+                src={organization.animalImageUrl}
+                alt={organization.animalName}
+              />
+              {/* <iframe width="696" height="346" src={organization.camUrl} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
+            </div>
+              <h5 className="card-title">
+                {organization.animalFunfact}
+              </h5>
+              <Link to={`/streams/organizations/${organization.id}`}>
+                <button className="btn btn-sm btn-outline-dark float-right">
+                  Live Stream
+                </button>
+              </Link>
+          </div>{" "}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-interface ISingleOrganizationProps extends RouteComponentProps<{id: string}> { }
+interface ISingleOrganizationProps
+  extends RouteComponentProps<{ id: string }> {}
 
-export default singleOrganizations
+export default singleOrganizations;
